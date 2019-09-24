42b264b3c573ced3b3480fc54fd87c38b910ac8e
sjleee05 <41768580+sjleee05@users.noreply.github.com>
2019-09-24 09:11:21 +0900

    Update nvidia-cuda10-0.sh

--- nvidia-cuda10-0.sh
+++ nvidia-cuda10-0.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Purge existign CUDA first
-sudo apt --purge remove "cublas*" "cuda*"
-sudo apt --purge remove "nvidia*"
+sudo apt --purge remove "cublas*" "cuda*" -y
+sudo apt --purge remove "nvidia*" -y
 
 # Install CUDA Toolkit 10
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub && sudo apt update
 sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 
-sudo apt update
+sudo apt update -y
 sudo apt install -y cuda
 
 # Install CuDNN 7 and NCCL 2
